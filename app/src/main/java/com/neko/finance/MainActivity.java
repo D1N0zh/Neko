5c0a637aea2d4263eae29c8110e1fc393e3a5c22
D1N0H <bongiorno.calogeroivan@gmail.com>
2026-08-26 19:37:55 +0200

    Polish Home Edit touch dragging

--- app/src/main/java/com/neko/finance/MainActivity.java
+++ app/src/main/java/com/neko/finance/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends Activity {
         });
 
         if (savedInstanceState == null) {
-            webView.loadUrl("file:///android_asset/index.html?v=signed111-home-edit-touch-drag");
+            webView.loadUrl("file:///android_asset/index.html?v=signed111-home-edit-drag-polish");
         } else {
             webView.restoreState(savedInstanceState);
         }
